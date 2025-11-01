6a6cabd2bd9cf82ea36aeb6e00bb6adf7bf3a363
Todibo99 <61287087+Todibo99@users.noreply.github.com>
2025-11-01 22:11:04 +0800

    update for multi image input

--- src/utils/vis_proto.py
+++ src/utils/vis_proto.py
@@ -30,9 +30,13 @@ def main():
         print(story.question, file=f)
 
     if len(story.reference_images) > 0:
-        for i in range(len(story.reference_images)):
-            with open(f"{output_path}/{i:03d}_reference_image.png", 'wb') as f:
-                f.write(story.reference_images[i].image.image_data)
+        if len(story.reference_images) == 1:
+            with open(f"{output_path}/000_reference_image.png", 'wb') as f:
+                f.write(story.reference_images[0].image.image_data)
+        else:
+            for i in range(len(story.reference_images)):
+                with open(f"{output_path}/000_reference_image_{i}.png", 'wb') as f:
+                    f.write(story.reference_images[i].image.image_data)
 
     idx = 1
     for c in story.clips:
